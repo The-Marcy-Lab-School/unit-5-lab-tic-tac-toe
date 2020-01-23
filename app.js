09259e8cd3e0698083f8faf1c67da353b966b29f
Enmanuel dlN <edelanuez.lang@gmail.com>
2020-01-23 18:11:41 +0000

    add moves to board

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const tableBody = document.getElementById('game-board');
+const gameMaster = new GameMaster()
+
+
 
 
 const tableCells = document.getElementsByTagName('td');
 for (let i = 0; i < tableCells.length; i++) {
     tableCells[i].addEventListener('click', () => {
-        console.log(`Clicked ${tableCells[i].innerHTML}`)
-        // gameMaster.fillCell()
-        // gameMaster.updateBoard
+        gameMaster.fillCell(tableCells[i], i);
+        gameMaster.updateBoard(tableCells);
+        console.log(gameMaster.matrix);
     })
 }
+
+// const clearButton = document.getElementById('clear');
+// clearButton.addEventListener('click', gameMaster.clearBoard(tableCells))
--- src/GameMaster.js
+++ src/GameMaster.js
@@ -1,11 +1,31 @@
 class GameMaster {
     constructor() {
-        this.isXNext = true;
+        this.moveCount = 0;
+        this.moves = '';
     }
 
-    fillCell() {}
+    fillCell(cell, cellNum) {
+        // if (this.matrix[cellNum == undefined]) {
+        cell.innerHTML = (this.moveCount % 2 === 0) ? `<i class="fas fa-times fa-2x"></i>` : `<i class="far fa-circle fa-2x"></i>`;
+        this.moveCount += 1;
 
-    updateBoard() {}
+        this.matrix[cellNum] = (this.moveCount % 2 === 0) ? 'o' : 'x';
+        // }
+    }
 
-    clearBoard() {}
+    updateBoard() {
+        //minimum number of moves to win
+        const cells = this.matrix
+        if (cells.length >= 5) {
+            if (cells[0] === cells[1] && cells[1] === cells[2]) {
+                console.log(`Player ${cells[0].toUpperCase()} wins!`)
+            }
+        }
+    }
+
+    clearBoard(cells) {
+        for (let cell = 0; cell < cells.length; cell++) {
+            cells[cell].innerHTML = '';
+        }
+    }
 }
